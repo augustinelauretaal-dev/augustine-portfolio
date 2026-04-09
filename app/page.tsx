4ce34b25380e4c35a0ec581f8383a9f03cf95881
augustinelauretaal-dev <augustine.lauretaal@gmail.com>
2026-04-09 15:45:47 +0800

    asdasd

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/Hero";
 import Services from "@/components/Services";
 import Projects from "@/components/Projects";
 import MissionVision from "@/components/MissionVision";
-import Clients from "@/components/Clients";
+{/* import Clients from "@/components/Clients"; */}
 import Faq from "@/components/Faq";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
